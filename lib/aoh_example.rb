7be5b37d619d4fd063da7d2c61982357699a0fbc
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2019-11-25 15:53:57 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,12 +25,11 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  return aoh[:row][:key]
 end
 
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
- 
+
 
 end
